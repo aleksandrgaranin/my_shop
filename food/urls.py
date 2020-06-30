d9bc0226eec9fb6cd41d9ac2446ef4bb2b578784
ALEKS G <aleksandrgaranin89@gmail.com>
2020-06-30 16:24:59 -0500

    added form to add item to db

--- food/urls.py
+++ food/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     #/food/id
     path('<int:item_id>/',views.detale,name='detail'),
     path('item/',views.item,name='item'),
+    # add item
+    path('add',views.create_item,name='create_item'),
 ]
 
